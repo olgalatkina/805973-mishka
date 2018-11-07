b672c6ed085460a53bc2ce8d7f8e058f48c30f40
Olga Latkina <olga.latkina@gmail.com>
2018-11-08 02:20:59 +0300

    попытка написать js

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,32 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+var promo = document.querySelector('.promo__button');
+// var product = document.querySelector('.product__buy-link');
+var modal = document.querySelector('.modal');
+var modalOverlay = document.querySelector(".modal__overlay");
+
+promo.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal--show");
+  modalOverlay.classList.add("modal__overlay--show");
+});
+
+// product.addEventListener("click", function (evt) { // срабатывает только для первой карточки
+//   evt.preventDefault();
+//   modal.classList.add("modal--show");
+//   modalOverlay.classList.add("modal__overlay--show");
+// });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains("modal--show")) {
+      modal.classList.remove("modal--show");
+    }
+    if (modalOverlay.classList.contains("modal__overlay--show")) {
+      modalOverlay.classList.remove("modal__overlay--show");
+    }
+  }
+});
